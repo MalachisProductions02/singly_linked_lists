bdbfaf95f7c3b374b46826edcbd60f38977b26df
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-02 01:46:34 +0000

    main

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/**
+ * size_t print_list - Functions that prints all elements for a list
+ * @list_t: The list
+ * @h: Parameter
+ * Return: If str is NULL, prints [0] (nil)
+ */
+
+size_t print_list(const list_t *h);
+
+int _putchar(char);
+
+#endif /* MAIN_H */
